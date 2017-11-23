ef15abf2478a0df74e265e26b3571bbfaa8fe416
Geleia <luizcanf@gmail.com>
2017-11-23 15:37:40 -0200

    Correção do BD no yoread-backup.sql

--- yoread-backup.sql
+++ yoread-backup.sql
@@ -18,15 +18,68 @@
 --
 -- Table structure for table `avalia`
 --
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `email` varchar(50) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `senha` varchar(30) DEFAULT NULL,
+  `nome` varchar(50) NOT NULL,
+  `sobrenome` varchar(50) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+-- publicacao 
+DROP TABLE IF EXISTS `publicacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publicacao` (
+  `data_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `email` varchar(50) NOT NULL,
+  `cod_publicacao` int(11) NOT NULL,
+  `sinopse` varchar(5000) DEFAULT NULL,
+  `titulo` varchar(80) DEFAULT NULL,
+  `link` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`cod_publicacao`),
+  KEY `email_fk` (`email`),
+  CONSTRAINT `email_fk` FOREIGN KEY (`email`) REFERENCES `users` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publicacao`
+--
+
+LOCK TABLES `publicacao` WRITE;
+/*!40000 ALTER TABLE `publicacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `publicacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 DROP TABLE IF EXISTS `avalia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `avalia` (
   `estrelas` int(11) DEFAULT NULL,
-  `cod_publicacao` int(11) DEFAULT NULL,
-  KEY `cod_publicacao_fk2` (`cod_publicacao`),
-  CONSTRAINT `cod_publicacao_fk2` FOREIGN KEY (`cod_publicacao`) REFERENCES `publicacao` (`cod_publicacao`)
+  `cod_publicacao` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`cod_publicacao`, `email`),
+  CONSTRAINT `cod_publicacao_fk2` FOREIGN KEY (`cod_publicacao`) REFERENCES `publicacao` (`cod_publicacao`),
+  KEY `email_fk2` (`email`),
+  CONSTRAINT `email_fk2` FOREIGN KEY (`email`) REFERENCES `users` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,13 +100,16 @@ DROP TABLE IF EXISTS `comenta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comenta` (
-  `data_hora` date DEFAULT NULL,
+  `data_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `descricao` varchar(2000) DEFAULT NULL,
-  `cod_publicacao` int(11) DEFAULT NULL,
+  `cod_publicacao` int(11) NOT NULL,
   `cod_comentario` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
   PRIMARY KEY (`cod_comentario`),
   KEY `cod_publicacao_fk` (`cod_publicacao`),
-  CONSTRAINT `cod_publicacao_fk` FOREIGN KEY (`cod_publicacao`) REFERENCES `publicacao` (`cod_publicacao`)
+  CONSTRAINT `cod_publicacao_fk` FOREIGN KEY (`cod_publicacao`) REFERENCES `publicacao` (`cod_publicacao`),
+  KEY `email_fk3` (`email`),
+  CONSTRAINT `email_fk3` FOREIGN KEY (`email`) REFERENCES `users` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,58 +122,7 @@ LOCK TABLES `comenta` WRITE;
 /*!40000 ALTER TABLE `comenta` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `publicacao`
---
 
-DROP TABLE IF EXISTS `publicacao`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `publicacao` (
-  `data_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `email` varchar(50) DEFAULT NULL,
-  `cod_publicacao` int(11) NOT NULL,
-  `sinopse` varchar(5000) DEFAULT NULL,
-  `titulo` varchar(80) DEFAULT NULL,
-  PRIMARY KEY (`cod_publicacao`),
-  KEY `email_fk` (`email`),
-  CONSTRAINT `email_fk` FOREIGN KEY (`email`) REFERENCES `usuario` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `publicacao`
---
-
-LOCK TABLES `publicacao` WRITE;
-/*!40000 ALTER TABLE `publicacao` DISABLE KEYS */;
-/*!40000 ALTER TABLE `publicacao` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
-  `email` varchar(50) NOT NULL,
-  `apelido` varchar(20) DEFAULT NULL,
-  `senha` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -128,3 +133,4 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2017-08-03  9:49:51
+
